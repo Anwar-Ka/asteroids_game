fdaf7d613130a2409b087dd3db1872b4c9245395
Anwar-Ka <anwarqssm@gmail.com>
2025-06-15 07:09:17 +0300

    Add destruction and split logic

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     our_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroids = pygame.sprite.Group()
     Asteroid.containers = (updatable, drawable, asteroids)
-    AsteroidField.containers = (updatable,)
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
     shots = pygame.sprite.Group()
     Shot.containers = (shots, updatable, drawable)
@@ -29,15 +29,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+                
         pygame.Surface.fill(screen, color = (0,0,0))
-        for sprite in drawable:
-            sprite.draw(screen)
         updatable.update(dt)
+        
         for asteroid in asteroids:
             if our_player.collides_with(asteroid):
                 print("Game over!")
                 pygame.quit()
                 exit()
+
+        for shot in shots:
+            for asteroid in asteroids:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         dt = fps.tick(60)/1000
 
